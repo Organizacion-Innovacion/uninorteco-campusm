a086d9863200f8b1e1b0bca8860c91b8cb32bf37
Blas Barrera <machaconb@uninorte.edu.co>
2024-06-26 09:31:29 -0500

    Visualización de errores de API

--- grades/src/client/components/Home.jsx
+++ grades/src/client/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { CircularProgress } from "@material-ui/core";
-import Container from "@material-ui/core/Container";
+import { CircularProgress, Container, Typography } from "@material-ui/core";
 import GradeCard from "./GradeCard";
 import SelectComponent from "./SelectComponent";
 import { fetchUserData, fetchUserTerms, fetchUserGrades } from "../utils/apiUtils";
@@ -13,6 +12,13 @@ const useStyles = makeStyles({
     alignItems: "center",
     height: "100vh",
   },
+  errorContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    color: "red",
+  },
 });
 
 const Screen = () => {
@@ -22,46 +28,58 @@ const Screen = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [terms, setTerms] = useState([]);
   const [user, setUser] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
+      setError('');
       try {
         const username = await fetchUserData();
         setUser(username);
+        setIsLoading(false);
       } catch (err) {
+        console.error('Error in fetchUserData:', err.message);
+        setError(err.message);
         setIsLoading(false);
       }
     };
-  
+
     fetchData();
   }, []);
-  
+
   useEffect(() => {
     const fetchTerms = async () => {
       if (user) {
         setIsLoading(true);
+        setError('');
         try {
           const termsResponse = await fetchUserTerms(user);
           setTerms(termsResponse);
           setSelectedTerm(termsResponse[0]?.PERIODO || '');
+          setIsLoading(false);
         } catch (err) {
+          console.error('Error in fetchUserTerms:', err.message);
+          setError(err.message);
           setIsLoading(false);
         }
       }
     };
-  
+
     fetchTerms();
   }, [user]);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
+      setError('');
       try {
         const subjectsGradesResponse = await fetchUserGrades(selectedTerm, user);
         setSubjectsGrades(subjectsGradesResponse);
         setIsLoading(false);
       } catch (err) {
+        console.error('Error in fetchUserGrades:', err.message);
+        setError(err.message);
         setIsLoading(false);
       }
     };
@@ -77,24 +95,32 @@ const Screen = () => {
 
   return (
     <Container>
-      <SelectComponent
-        value={selectedTerm}
-        onChange={handleSelectChange}
-        options={terms}
-      />
-      {isLoading ? (
-        <div className={classes.loaderContainer}>
-          <CircularProgress />
+      {error ? (
+        <div className={classes.errorContainer}>
+          <Typography variant="h6">{error}</Typography>
         </div>
       ) : (
-        subjectsGrades.map((subjectGrade) => (
-          <GradeCard
-            key={subjectGrade.materia}
-            gradeName={subjectGrade.materia}
-            items={subjectGrade.items}
-            className={classes.container}
+        <>
+          <SelectComponent
+            value={selectedTerm}
+            onChange={handleSelectChange}
+            options={terms}
           />
-        ))
+          {isLoading ? (
+            <div className={classes.loaderContainer}>
+              <CircularProgress />
+            </div>
+          ) : (
+            subjectsGrades.map((subjectGrade) => (
+              <GradeCard
+                key={subjectGrade.materia}
+                gradeName={subjectGrade.materia}
+                items={subjectGrade.items}
+                className={classes.container}
+              />
+            ))
+          )}
+        </>
       )}
     </Container>
   );
