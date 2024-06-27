9ce4c3e94f6bd3568646605f5d9fbeb71855abcd
Blas Barrera <machaconb@uninorte.edu.co>
2024-06-27 16:07:28 -0500

    Recuperar diseño antiguo & mejorado

--- pc-availables/src/client/disponibilidad-equipos-y-salas/components/SalaUsuarioList.jsx
+++ pc-availables/src/client/disponibilidad-equipos-y-salas/components/SalaUsuarioList.jsx
@@ -1,71 +1,94 @@
 import React, { useState, useEffect } from 'react';
+import { Typography, Grid, Container, Alert, CircularProgress, Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import SalaUsuarioItem from './SalaUsuarioItem';
 import { fetchData } from '../../../services/get-data';
-import { Grid, Typography, CircularProgress, Box } from '@mui/material';
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import RoomIcon from '@mui/icons-material/Room';
 
+const useStyles = makeStyles(() => ({
+  container: {
+    backgroundColor: '#e0e0e0',
+    padding: '20px',
+    minHeight: '100vh',
+  },
+  alert: {
+    margin: '20px 0',
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '20px',
+    padding: '10px',
+    backgroundColor: '#960A11',
+    color: '#fff',
+    borderRadius: '5px',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+  },
+  icon: {
+    marginRight: '10px',
+    fontSize: '40px',
+  },
+  title: {
+    fontSize: '2rem',
+  },
+}));
+
 const SalaUsuarioList = () => {
-  const [salas, setSalas] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [salasUsuario, setSalasUsuario] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const classes = useStyles();
 
   useEffect(() => {
-    fetchData()
-      .then(data => {
-        setSalas(data);
-        setLoading(false);
-        setError(null); // Limpiar cualquier error previo al cargar correctamente
-      })
-      .catch(err => {
-        setError(err);
-        setLoading(false);
-      });
+    obtenerDatosSalaUsuario();
   }, []);
 
-  if (loading) {
-    return (
-      <div style={{ textAlign: 'center', margin: '20px' }}>
-        <CircularProgress />
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <Box sx={{ backgroundColor: '#ffffff', color: '#1d1d1b', textAlign: 'center', padding: '20px', marginBottom: 2, boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-        <ErrorOutlineIcon style={{ fontSize: 40, marginBottom: -8, marginRight: 10, verticalAlign: 'middle' }} />
-        <Typography variant="h5" component="h1" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-          Error al cargar las salas
-        </Typography>
-      </Box>
-    );
-  }
-
-  if (salas.length === 0) {
-    return (
-      <Typography variant="h6" style={{ margin: '20px' }}>
-        No hay datos de salas disponibles
-      </Typography>
-    );
-  }
+  const obtenerDatosSalaUsuario = () => {
+    setLoading(true);
+    fetchData()
+      .then((data) => {
+        setSalasUsuario(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  };
 
   return (
-    <>
-      <Box sx={{ backgroundColor: '#1d1d1b', color: '#fff', textAlign: 'center', padding: '20px', marginBottom: 2, boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-        <RoomIcon style={{ fontSize: 40, marginBottom: -8, marginRight: 10, verticalAlign: 'middle' }} />
-        <Typography variant="h4" component="h1" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+    <Container className={classes.container}>
+      <Box className={classes.header}>
+        <RoomIcon className={classes.icon} />
+        <Typography variant="h4" component="h1" className={classes.title}>
           Lista de Salas disponibles
         </Typography>
       </Box>
-      <Grid container spacing={1}>
-        {salas.map(sala => (
-          <Grid item key={sala.id} xs={6}>
-            <SalaUsuarioItem sala={sala} />
+      {loading && (
+        <div style={{ textAlign: 'center', margin: '20px' }}>
+          <CircularProgress />
+        </div>
+      )}
+      {error && (
+        <Alert severity="error" className={classes.alert}>
+          {error}
+        </Alert>
+      )}
+      {!loading && !error && salasUsuario.length === 0 && (
+        <Typography variant="h6" style={{ margin: '20px' }}>
+          No hay datos de salas disponibles
+        </Typography>
+      )}
+      <Grid container spacing={2}>
+        {salasUsuario.map((sala, index) => (
+          <Grid item xs={6} key={index}>
+            <SalaUsuarioItem sala={sala} isRight={index % 2 !== 0} />
           </Grid>
         ))}
       </Grid>
-    </>
+    </Container>
   );
 };
 
